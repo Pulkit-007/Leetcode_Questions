8d597827a6c63416695ff23f0c4c6882434eb4d3
Pulkit Sharma <91002702+Pulkit-007@users.noreply.github.com>
2023-01-02 09:58:56 +0530

    Update 520. Detect Capital.java

--- January2023/520. Detect Capital.java
+++ January2023/520. Detect Capital.java
@@ -1,1 +1,19 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.length()<2) return true;
+        int upper=0;
+        for(int i=0;i<word.length();i++)
+        {
+            if(word.charAt(i)>='A' && word.charAt(i)<='Z')
+            {
+                upper++;
+            }
+        }
+        if(upper==word.length()) return true;
+        if(upper==0) return true;
+        if(upper==1 && word.charAt(0)>='A' && word.charAt(0)<='Z') return true;
+        return false;
 
+        
+    }
+}
