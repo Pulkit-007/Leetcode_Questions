1a3a3f0f2ef93b43d18fd3808b337841d283f8c6
Pulkit Sharma <91002702+Pulkit-007@users.noreply.github.com>
2023-01-03 22:38:49 +0530

    Create 944. Delete Columns to Make Sorted.java

---
+++ January2023/944. Delete Columns to Make Sorted.java
@@ -0,0 +1,16 @@
+class Solution {
+        public int minDeletionSize(String[] arr) {
+        int c = 0;
+        for(int i=0; i<arr[0].length(); i++){
+            for(int j=1; j<arr.length; j++){
+                if(arr[j].charAt(i) < arr[j-1].charAt(i)){
+                    c++;
+                    break;
+            }
+        }
+    }     
+    return c;
+
+        
+    }
+}
