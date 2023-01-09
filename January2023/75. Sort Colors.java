9a35d7602c40668c6dee4b87a5b828e80f25ddfc
Pulkit Sharma <91002702+Pulkit-007@users.noreply.github.com>
2023-01-09 19:51:35 +0530

    Create 75. Sort Colors.java

---
+++ January2023/75. Sort Colors.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void sortColors(int[] nums) {
+      //Naive Approach
+        // Arrays.sort(nums);
+      
+      
+      
+      //Optimal Solution 
+      //Dutch National Flag Algorithm
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        int temp;
+        while(mid<=high)
+        switch(nums[mid])
+        {
+            case 0: {
+                temp=nums[mid];
+                nums[mid]=nums[low];
+                nums[low]=temp;
+                low++;
+                mid++;
+                break;
+            }
+            case 1: {
+                mid++;
+                break;
+
+            }
+            case 2: {
+                temp=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
+                high--;
+                break;
+            }
+        }
+        
+    }
+}
