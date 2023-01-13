4e323431207194c84bb04555e80dfc45ac7248f5
Pulkit Sharma <91002702+Pulkit-007@users.noreply.github.com>
2023-01-13 14:24:15 +0530

    Create 1342. Number of Steps to Reduce a Number to Zero.java

---
+++ January2023/1342. Number of Steps to Reduce a Number to Zero.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        int c=0;
+        while(num!=0)
+        {
+            num=num%2==0?num/2:num-1;
+            c++;
+        }
+        return c;
+
+        
+        
+    }
+}
