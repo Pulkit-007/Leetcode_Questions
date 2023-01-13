c11bee37defcb7ad048a76d1ce6eb5fff9b504ad
Pulkit Sharma <91002702+Pulkit-007@users.noreply.github.com>
2023-01-13 13:40:52 +0530

    Create 1323. Maximum 69 Number.java

---
+++ January2023/1323. Maximum 69 Number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int maximum69Number (int num) {
+        String s=String.valueOf(num);
+        s=s.replaceFirst("6","9");
+        return Integer.parseInt(s);
+        
+    }
+}
