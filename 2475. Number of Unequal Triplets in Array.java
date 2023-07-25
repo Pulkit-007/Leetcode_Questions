280efe92a9110c0ffd3a17201766fba61caab09e
Pulkit Sharma <91002702+Pulkit-007@users.noreply.github.com>
2023-07-25 09:51:58 +0530

    Number of Unequal Triplets in Array

---
+++ 2475. Number of Unequal Triplets in Array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int unequalTriplets(int[] nums) {
+        int cnt=0;
+        for(int i=0;i<nums.length-2;i++)
+        {
+            for(int j=i+1;j<nums.length-1;j++)
+            {
+                for(int k=j+1;k<nums.length;k++)
+                {
+                    if(nums[i]!=nums[j] && nums[j]!=nums[k] && nums[i]!=nums[k])
+                    {
+                        cnt++;
+                    }
+                }
+            }
+        }
+        return cnt;
+
+        
+    }
+}
