db5117e2cf1d4830ab7d1ec7c69b16b3a50feaf5
Pulkit Sharma <pulkits94125@gmail.com>
2023-08-31 14:30:57 +0530

    Sum of Squares of Special Elements

---
+++ 2778. Sum of Squares of Special Elements.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int sumOfSquares(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(n%(i+1)==0)
+            {
+                sum+=nums[i]*nums[i];
+            }
+        }
+        return sum;
+        
+    }
+}
