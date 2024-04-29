e9ac0af2f2b21889561e0ef272b5ce04da489f7e
Pulkit Sharma <pulkits94125@gmail.com>
2024-04-30 00:53:17 +0530

    Single Number II

---
+++ 137. Single Number II.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i+=3)
+        {
+            if(nums[i]!=nums[i-1])
+            {
+                return nums[i-1];
+            }
+        }
+        return nums[nums.length-1];
+        
+    }
+}
