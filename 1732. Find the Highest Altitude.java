ab32d699617f5805192358cbf0466f0ad276a0b9
Pulkit Sharma <pulkits94125@gmail.com>
2024-05-01 21:08:51 +0530

    Find the Highest Altitude

---
+++ 1732. Find the Highest Altitude.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max=0;
+        int alt=0;
+        for(int i=0;i<gain.length;i++)
+        {
+            alt+=gain[i];
+            if(alt>max)
+            {
+                max=alt;
+            }
+        }
+        return max;
+
+        
+        
+    }
+}
