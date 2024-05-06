8bb113ccc0343795d4009765118df06b30cc5f64
Pulkit Sharma <pulkits94125@gmail.com>
2024-05-06 22:02:18 +0530

    Binary Search

---
+++ 704. Binary Search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int binarySearch(int[] nums, int low, int high, int target) {
+        if (low > high) return -1; //Base case.
+
+        // Perform the steps:
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return mid;
+        else if (target > nums[mid])
+            return binarySearch(nums, mid + 1, high, target);
+        return binarySearch(nums, low, mid - 1, target);
+    }
+    public int search(int[] nums, int target) {
+         return binarySearch(nums, 0, nums.length - 1, target);
+        
+    }
+}
