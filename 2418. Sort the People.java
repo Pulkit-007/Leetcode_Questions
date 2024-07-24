369f70ca330c1e5f06f21bb0d329087979084dbd
Pulkit Sharma <pulkits94125@gmail.com>
2024-07-24 18:40:26 +0530

    Sort the People

---
+++ 2418. Sort the People.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        int n=names.length;
+        Map<Integer,String> map=new HashMap<>();
+        for(int i=0;i<n;i++)
+        {
+            map.put(heights[i],names[i]);
+        }
+        Arrays.sort(heights);
+        for (int i = 0; i < n / 2; ++i) {
+            int temp = heights[i];
+            heights[i] = heights[n - 1 - i];
+            heights[n - 1 - i] = temp;
+        }
+        for(int i=0;i<n;i++)
+        {
+            names[i]=map.get(heights[i]);
+        }
+        return names;
+        
+    }
+}
