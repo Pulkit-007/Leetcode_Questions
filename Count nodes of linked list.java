a78d17459d02c27c4d024b2c35a01675105ab259
Pulkit Sharma <pulkits94125@gmail.com>
2024-08-25 12:19:41 +0530

    Count nodes of linked list

---
+++ Count nodes of linked list.java
@@ -0,0 +1,43 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+    
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+    
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+    
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+public class Solution {
+    public static int length(Node head){
+        //Your code goes here
+        Node temp=head;
+        int cnt=0;
+        while(temp!=null)
+        {
+            temp=temp.next;
+            cnt++;
+        }
+        return cnt;
+
+    }
+}
