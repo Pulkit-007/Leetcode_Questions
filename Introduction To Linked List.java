32dc8b787be544ba0fb0c996f8e9c7fc8b808af4
Pulkit Sharma <pulkits94125@gmail.com>
2024-08-25 12:18:32 +0530

    Introduction To Linked List

---
+++ Introduction To Linked List.java
@@ -0,0 +1,44 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution {
+    public static Node constructLL(int []arr) {
+        // Write your code here
+        Node head=new Node(arr[0]);
+        Node mover=head;
+        for(int i=1;i<arr.length;i++)
+        {
+            Node temp=new Node(arr[i]);
+            mover.next=temp;
+            mover=temp;
+        }
+        return head;
+        
+    }
+}
