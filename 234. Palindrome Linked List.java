32dcec9558011aa11378c60055d9538baa79fd8e
Pulkit Sharma <pulkits94125@gmail.com>
2024-09-02 17:58:33 +0530

    Palindrome Linked List

---
+++ 234. Palindrome Linked List.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        Stack<Integer> st=new Stack<>();
+        ListNode temp=head;
+        while(temp!=null)
+        {
+            st.push(temp.val);
+            temp=temp.next;
+        }
+        temp=head;
+        while(temp!=null)
+        {
+            if(temp.val!=st.peek())
+            {
+                return false;
+            }
+            st.pop();
+            temp=temp.next;
+        }
+        return true;
+        
+    }
+}
