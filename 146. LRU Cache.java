3bd9d3a8ea6d55b464c44d3a3e93481a3f99eacc
Pulkit Sharma <pulkits94125@gmail.com>
2024-10-14 10:29:47 +0530

    LRU Cache

---
+++ 146. LRU Cache.java
@@ -0,0 +1,57 @@
+class LRUCache {
+    Node head=new Node(0,0);
+    Node tail=new Node(0,0);
+    Map<Integer,Node> map=new HashMap<>();
+    int n=0;
+
+    public LRUCache(int capacity) {
+        n=capacity;
+        head.next=tail;
+        tail.prev=head;
+    }
+    
+    public int get(int key) {
+        if(map.containsKey(key)){
+            Node node=map.get(key);
+            remove(node);
+            insert(node);
+            return node.value;
+        }
+        else{
+            return -1;
+        }
+    }
+    
+    public void put(int key, int value) {
+        if(map.containsKey(key)){
+            remove(map.get(key));
+        }
+        if(map.size()==n){
+            remove(tail.prev);
+        }
+        insert(new Node(key,value));
+    }
+
+    public void insert(Node node){
+        map.put(node.key,node);
+        node.next=head.next;
+        node.next.prev=node;
+        head.next=node;
+        node.prev=head;
+    }
+
+    public void remove(Node node){
+        map.remove(node.key);
+        node.prev.next=node.next;
+        node.next.prev=node.prev;
+    }
+
+    class Node{
+        Node prev,next;
+        int key,value;
+        Node(int k,int v){
+            key=k;
+            value=v;
+        }
+    }
+}
