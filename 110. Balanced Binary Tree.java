0a6bc9c516fd52bd1c22d2aa7471b3ee23a936d6
Pulkit Sharma <pulkits94125@gmail.com>
2024-10-23 12:20:42 +0530

    Balanced Binary Tree

---
+++ 110. Balanced Binary Tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfsHeight(root)!=-1;
+        
+    }
+    public static int dfsHeight(TreeNode root)
+    {
+        if(root==null) return 0;
+        int lh=dfsHeight(root.left);
+        int rh=dfsHeight(root.right);
+        if(rh==-1 || lh==-1)
+        {
+            return -1;
+        }
+        if(Math.abs(lh-rh)>1) return -1;
+        return 1+Math.max(lh,rh);
+    }
+}
