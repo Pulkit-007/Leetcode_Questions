bd2f6a318f293826075f02aa067969603367ddd4
Pulkit Sharma <pulkits94125@gmail.com>
2024-11-10 20:30:52 +0530

    Count Complete Tree Nodes

---
+++ 222. Count Complete Tree Nodes.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root==null) return 0;
+        int lh = findHeightLeft(root);
+        // Find the height of the right subtree
+        int rh = findHeightRight(root);
+        if (lh == rh) {
+            return (1 << lh) - 1;
+        }
+        return 1 + countNodes(root.left) + countNodes(root.right);
+        
+    }
+    private int findHeightLeft(TreeNode node) {
+        int height = 0;
+        while (node != null) {
+            height++;
+            node = node.left;
+        }
+        return height;
+    }
+
+    // Function to find the
+    // height of the right subtree
+    private int findHeightRight(TreeNode node) {
+        int height = 0;
+        while (node != null) {
+            height++;
+            node = node.right;
+        }
+        return height;
+    }
+
+}
