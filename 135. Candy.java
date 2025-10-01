54dbacbe4c329f1e3849adc44e05111ed85ebf46
Pulkit Sharma <pulkits94125@gmail.com>
2025-10-02 04:15:58 +0530

    Candy

---
+++ 135. Candy.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int [] left=new int[n];
+        int [] right=new int[n];
+        left[0]=1;
+        right[n-1]=1;
+        for(int i =1;i<n;i++)
+        {
+            if(ratings[i]>ratings[i-1])
+            {
+                left[i]=left[i-1]+1;
+
+            }
+            else
+            {
+                left[i]=1;
+            }
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            if(ratings[i]>ratings[i+1])
+            {
+                right[i]=right[i+1]+1;
+
+            }
+            else
+            {
+                right[i]=1;
+            }
+        }
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+Math.max(left[i],right[i]);
+        }
+        return sum;
+        
+    }
+}
